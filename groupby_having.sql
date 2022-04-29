775dca3f177bcbc0636d33feddf0a9dbbb423295
Apurba Kumar Majumder <9851apurba@gmail.com>
2022-04-29 23:45:20 +0530

    starting with having condition

--- groupby_having.sql
+++ groupby_having.sql
@@ -6,6 +6,17 @@ GROUP BY designation;
 
 -- 2. if you are group by : column aggregate data get -> get grouped
 SELECT designation, Count(designation) as "count per dept",
-MAX(Salary) from employee
+MAX(Salary) as "Max Salary" from employee
 -- i will make group on the basis of given column
 GROUP BY designation; 
+
+-- group by -> to form formal groups
+--          -> to apply aggregation on these groups
+
+-- Having: it is always used with a aggregate function
+--         used to add condition on group by(group data)
+--         therefore it comes after group by statement
+
+SELECT designation FROM employee
+GROUP BY designation
+HAVING MAX(Salary) > 3000;
