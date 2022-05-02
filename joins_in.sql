e7c4c61af3a87a93ea8610fb667018abf6eee2da
Apurba Kumar Majumder <9851apurba@gmail.com>
2022-05-02 11:49:09 +0530

    left join and right join

--- joins_in.sql
+++ joins_in.sql
@@ -20,3 +20,71 @@ SELECT * FROM DEPARTMENT
 INNER JOIN employee
 ON
 employee.DEPTCODE = DEPARTMENT.DEPTCODE; 
+
+-- Inner Join to get all the results
+-- select everything from employe
+-- SELECT * FROM department 
+-- -- and while selecting from employee make a inner join with Department
+-- INNER JOIN employee
+-- -- on which column we need to  make intersection 
+-- --  on intersection corresponding rows of both the table will be joined
+-- ON DEPARTMENT.DEPTCODE=employee.DEPTCODE;
+
+-- to get my  result in desired  order column wise 
+
+SELECT * FROM department 
+-- and while selecting from employee make a inner join with Department
+INNER JOIN employee
+-- on which column we need to  make intersection 
+--  on intersection corresponding rows of both the table will be joined
+ON DEPARTMENT.DEPTCODE=employee.DEPTCODE;
+
+-- to get you result in a given order
+
+SELECT employee.DEPTCODE,department.DEPTCODE,employee.EmpFName,department.LOCATION
+from employee
+INNER JOIN department
+ON employee.DEPTCODE=department.DEPTCODE;
+-- list of employees who belong a valid particular department
+
+
+-- LEFT JOIN 
+SELECT employee.DEPTCODE,department.DEPTCODE,employee.EmpFName,department.LOCATION
+from employee
+LEFT JOIN department
+ON employee.DEPTCODE=department.DEPTCODE;
+
+
+-- right Join
+SELECT employee.DEPTCODE,department.DEPTCODE,employee.EmpFName,department.LOCATION
+from employee
+RIGHT JOIN department
+ON employee.DEPTCODE=department.DEPTCODE;
+
+
+
+-- full outer join 
+
+-- LEFT JOIN 
+SELECT employee.DEPTCODE,department.DEPTCODE,employee.EmpFName,department.LOCATION
+from employee
+LEFT JOIN department
+ON employee.DEPTCODE=department.DEPTCODE
+UNION
+-- right Join
+SELECT employee.DEPTCODE,department.DEPTCODE,employee.EmpFName,department.LOCATION
+from employee
+RIGHT JOIN department
+ON employee.DEPTCODE=department.DEPTCODE;
+© 2022 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
